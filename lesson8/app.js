62029edee0badc28b035fa9a2b202cdcf61ffd95
farhanacsebd <farhana.cse.bd@gmail.com>
2023-08-13 23:45:34 +0600

    express server done

--- lesson8/app.js
+++ lesson8/app.js
@@ -1,5 +1,26 @@
 const express = require("express");
-const app = express;
+const app = express();
 
 
-module.exports = app;
+app.get("/", (req,res) => {
+    res.send('I am get request at home router');
+    res.end();
+});
+
+app.post("/", (req,res) => {
+    res.send('I am post request at home router');
+    res.end();
+});
+
+app.put("/", (req,res) => {
+    res.send('I am put request at home router');
+    res.end();
+});
+
+app.delete("/", (req,res) => {
+    res.send('I am delete request at home router');
+    res.end();
+});
+
+
+module.exports = app; 
