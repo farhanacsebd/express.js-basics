eff67d0ba3f874b58e53bfa25b7995d41dcb8022
farhanacsebd <farhana.cse.bd@gmail.com>
2023-08-30 23:41:16 +0600

    send and receive form data

--- lesson9.1/index.js
+++ lesson9.1/index.js
@@ -6,11 +6,23 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/user",(req,res) =>{
+app.get("/register",(req,res) =>{
+    
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/register",(req,res) =>{
+    const fullName = req.body.fullName;
+    const age = req.body.age;
+
+    res.send(`<h1>Your name is ${fullName} and age is ${age}</h1>`)
+})
+
+/* app.post("/user",(req,res) =>{
     const name = req.body.name;
 
     res.send(`Welcome ${name}`)
-})
+}) */
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
