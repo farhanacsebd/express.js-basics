faf5d1bd780150e0e02942397d6955069dd25bd4
farhanacsebd <farhana.cse.bd@gmail.com>
2023-08-31 23:19:29 +0600

    area calculator

--- AreaCalculator/index.js
+++ AreaCalculator/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.get("/",(req,res) =>{
-    res.send('hello')
+    res.sendFile(__dirname + "/views/index.html")
+})
+app.get("/circle",(req,res) =>{
+    res.sendFile(__dirname + "/views/circle.html")
+})
+app.get("/triangle",(req,res) =>{
+    res.sendFile(__dirname + "/views/triangle.html")
+})
+
+
+app.post("/triangle",(req,res) =>{
+    const height = req.body.height;
+    const base = req.body.base;
+    const area = 0.5 * height * base;
+
+    res.send(`<h2>Area of Triangle ${area}</h2>`)
+})
+
+app.post("/circle",(req,res) =>{
+    const radius = req.body.radius;
+    const area = Math.PI * radius * radius;
+
+    res.send(`<h2>Area of Circle ${area}</h2>`)
 })
 
 app.listen(PORT,() =>{
